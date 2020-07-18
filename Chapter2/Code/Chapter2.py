22c4790614e17a96485a650965eb848d839ea73e
Michael-cuhk <michael.yunfei@gmail.com>
2020-07-18 15:46:15 +0100

    update chapter 2 again

--- Chapter2/Code/Chapter2.py
+++ Chapter2/Code/Chapter2.py
@@ -22,6 +22,7 @@ iris = datasets.load_iris()
 iris.data
 iris.target
 iris_df = pd.DataFrame(iris.data, columns=iris.feature_names)
+iris_df.shape
 
 wine = datasets.load_wine()
 wine.data
@@ -98,12 +99,44 @@ bmi_plotly_fig = px.scatter_3d(women_men_df2, x='height',
                                z='gender', color='gender')
 bmi_plotly_fig.show()
 
+# working with iris dataset again
+iris = datasets.load_iris()
+iris_df2 = pd.DataFrame(np.column_stack([iris.data, iris.target]),
+                        columns=iris.feature_names + ['category'])
+iris_df2.category = iris_df2.category.astype(int)
+iris_df2.head()
+iris_df.shape
 
+# build up 3 equations
+for i in range(3):
+    print(iris_df2[iris_df2['category'] == i].iloc[1, :])
 
+# solve the the linear equations
+iris_mat = np.array([[3.0, 1.4, 0.2],
+                     [3.2, 4.5, 1.5],
+                     [2.7, 5.1, 1.9]])
+iris_param = np.linalg.inv(iris_mat) @ np.asmatrix([0, 1, 2]).reshape((3, 1))
+print(iris_param)
 
+# 1.1， -3.7, 9.4
+iris_df2.iloc[13, :]  # categor 0
+iris_df2.iloc[13, 1:4] @ iris_param
 
+# use the parameters from three equations to predict
+iris_df2_pred = np.asmatrix(iris_df2.iloc[:, 1:4] @ iris_param)
+iris_df2_pred_cat = np.ones((iris_df2_pred.shape))*9
+iris_df2_pred_cat.astype(int)
+# classifying the dataset
+for i in range(iris_df2_pred.shape[0]):
+    if iris_df2_pred[i, :] <= 0.2:
+        iris_df2_pred_cat[i, :] = 0
+    elif iris_df2_pred[i, :] <= 1.2:
+        iris_df2_pred_cat[i, :] = 1
+    elif iris_df2_pred[i, :] <= 2.2:
+        iris_df2_pred_cat[i, :] = 2
 
-
+iris_check = np.sum(iris_df2_pred_cat.astype(int).ravel() == iris_df2.category)
+print(iris_check/iris_df2.shape[0])
 
 
 
